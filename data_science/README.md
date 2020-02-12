7699c7f858e8264e5b46d2e8096dbb6128711409
Oana <oignat@umich.edu>
2020-02-12 17:20:24 -0500

    readme + knn

--- data_science/README.md
+++ data_science/README.md
@@ -1,9 +1,29 @@
 ## Classification algorithms: 
-- [x] Linear: Logistic Regression, Naive Bayes
-- [x] Nearest Neighbour
+- [x] Logistic Regression
+- [x] Linear Regression
 - [x] SVM 
+- [x] K Nearest Neighbours
+    - As we decrease the value of K, our predictions become less stable
+    - The algorithm gets significantly **slower** as the number of examples and/or features/independent variables increase
+    - An application of KNN-search: recommender systems, where we have computing resources to speedily handle the data 
+- [x] Naive Bayes
+    - They are **extremely fast** for both training and prediction
+    - They provide straightforward probabilistic prediction
+    - They are often very easily **interpretable**
+    - They have **very few (if any) tunable parameters**
+    - Works well when the **naive assumptions actually match the data** (very rare in practice)
+    - Works well for very **well-separated categories**, when model complexity is less important
+    - Works well for very **high-dimensional data**, when model complexity is less important
 - [x] Decision Trees
+    - Use **Entropy** and **Information Gain**
+    - Computationally cheap to use 
+    - Easy for humans to **understand results**
+    - It can deal with irrelevant features
+    - Prone to **overfitting**
 - [x] Neural Networks
+    - Very effective for **high dimensionality** problems 
+    - Able to deal with **complex relations between variables**, non-exhaustive category sets  
+    - Powerful **tuning options to prevent over- and under-fitting**
 
 ## Multimodal Methods:
 - [x] LXMERT
---
+++ data_science/knn.py
@@ -0,0 +1,109 @@
+import math
+from collections import Counter
+
+
+def mean(labels):
+    sum = 0
+    for i in labels:
+        sum += i
+    return sum / len(labels)
+
+
+def mode(labels):
+    dict_nb = Counter(labels)
+    return (dict_nb.most_common(1)[0][0])
+
+
+def euclidean_distance(point1, point2):
+    sum = 0
+    for i in range(len(point1)):
+        sum += (point1[i] - point2[i]) ** 2
+    return math.sqrt(sum)
+
+
+def knn(data, query, k, distance_fn, choice_fn):
+    neighbor_distances_and_indices = []
+
+    # 3. For each example in the data
+    for index, example in enumerate(data):
+        # 3.1 Calculate the distance between the query example and the current
+        # example from the data.
+        distance = distance_fn(example[:-1], query)
+
+        # 3.2 Add the distance and the index of the example to an ordered collection
+        neighbor_distances_and_indices.append((distance, index))
+
+    # 4. Sort the ordered collection of distances and indices from
+    # smallest to largest (in ascending order) by the distances
+    sorted_neighbor_distances_and_indices = sorted(neighbor_distances_and_indices)
+
+    # 5. Pick the first K entries from the sorted collection
+    k_nearest_distances_and_indices = sorted_neighbor_distances_and_indices[:k]
+
+    # 6. Get the labels of the selected K entries
+    k_nearest_labels = [data[i][1] for distance, i in k_nearest_distances_and_indices]
+
+    # 7. If regression (choice_fn = mean), return the average of the K labels
+    # 8. If classification (choice_fn = mode), return the mode of the K labels
+    return choice_fn(k_nearest_labels)
+
+
+def main():
+    '''
+    # Regression Data
+    #
+    # Column 0: height (inches)
+    # Column 1: weight (pounds)
+    '''
+    reg_data = [
+        [65.75, 112.99],
+        [71.52, 136.49],
+        [69.40, 153.03],
+        [68.22, 142.34],
+        [67.79, 144.30],
+        [68.70, 123.30],
+        [69.80, 141.49],
+        [70.01, 136.46],
+        [67.90, 112.37],
+        [66.49, 127.45],
+    ]
+
+    # Question:
+    # Given the data we have, what's the best-guess at someone's weight if they are 60 inches tall?
+    reg_query = [60]
+    reg_prediction = knn(
+        reg_data, reg_query, k=3, distance_fn=euclidean_distance, choice_fn=mean)
+    print("if they are 60 inches tall, their weigth is probably " + str(reg_prediction))
+    '''   
+    # Classification Data
+    # 
+    # Column 0: age
+    # Column 1: likes pineapple
+    '''
+    clf_data = [
+        [22, 1],
+        [23, 1],
+        [21, 1],
+        [18, 1],
+        [19, 1],
+        [25, 0],
+        [27, 0],
+        [29, 0],
+        [31, 0],
+        [45, 0],
+    ]
+    # Question:
+    # Given the data we have, does a 33 year old like pineapples on their pizza?
+    clf_query = [33]
+    clf_prediction = knn(
+        clf_data, clf_query, k=3, distance_fn=euclidean_distance, choice_fn=mode
+    )
+
+    if clf_prediction == 0:
+        print("No, a 33 year old does not like pineapples on their pizza")
+    else:
+        print("Yes, a 33 year old likes pineapples on their pizza")
+
+
+if __name__ == '__main__':
+    main()
