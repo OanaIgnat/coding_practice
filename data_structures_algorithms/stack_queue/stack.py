91d8caafd0ca523593cd744a937f41d78ef8f649
Oana Ignat <39847267+OanaIgnat@users.noreply.github.com>
2020-02-13 11:08:23 -0500

    Update stack.py

--- data_structures_algorithms/stack_queue/stack.py
+++ data_structures_algorithms/stack_queue/stack.py
@@ -1,19 +1,44 @@
 from collections import deque
 
 
-# def is_array_stack_permutation(input1, input2):
-#     length = len(input2)
-#     if input1[-1] == input2[0]:
-#
-#         for i in range(length-1):
-#             if input1[i] != input2[length-i]:
-#                 print("False")
-#                 break
-#         print("True")
-#         return
-#     else:
-
-# def parathesis_checker(exp):
+# Python program for implementation of stack 
+# import maxsize from sys module  
+# Used to return -infinite when stack is empty 
+from sys import maxsize 
+  
+# Function to create a stack. It initializes size of stack as 0 
+def createStack(): 
+    stack = [] 
+    return stack 
+  
+# Stack is empty when stack size is 0 
+def isEmpty(stack): 
+    return len(stack) == 0
+  
+# Function to add an item to stack. It increases size by 1 
+def push(stack, item): 
+    stack.append(item) 
+    print(item + " pushed to stack ") 
+      
+# Function to remove an item from stack. It decreases size by 1 
+def pop(stack): 
+    if (isEmpty(stack)): 
+        return str(-maxsize -1) # return minus infinite 
+      
+    return stack.pop() 
+  
+# Function to return the top from stack without removing it 
+def peek(stack): 
+    if (isEmpty(stack)): 
+        return str(-maxsize -1) # return minus infinite 
+    return stack[len(stack) - 1] 
+  
+# Driver program to test above functions     
+stack = createStack() 
+push(stack, str(10)) 
+push(stack, str(20)) 
+push(stack, str(30)) 
+print(pop(stack) + " popped from stack") 
 
 
 # reverse a string without stack
