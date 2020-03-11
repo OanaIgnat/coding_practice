4c786fa0bdfac1e99499dd78eb888b6c0250b897
Oana <oignat@umich.edu>
2020-03-11 01:49:21 -0400

    kmeans

--- data_science/kmeans.py
+++ data_science/kmeans.py
@@ -2,13 +2,16 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from matplotlib import style
+
 style.use('ggplot')
+
 
 def euclidean_distance(point1, point2):
     return math.sqrt(sum((point1 - point2) ** 2))
 
-def kmeans(points,nb_iterations, k):
-    centroids = np.zeros((k, points.shape[1])) # (#points, #coordinates)
+
+def kmeans(points, nb_iterations, k):
+    centroids = np.zeros((k, points.shape[1]))  # (#points, #coordinates)
     for i in range(nb_iterations):
         dict_centroid_points = {}
         for p in points:
@@ -24,9 +27,10 @@ def kmeans(points,nb_iterations, k):
             dict_centroid_points[c_min].append(p)
 
         for c in dict_centroid_points.keys():
-            new_centroid = np.mean(dict_centroid_points[c], axis = 0)
+            new_centroid = np.mean(dict_centroid_points[c], axis=0) # mean over the coordinates
             centroids[c] = new_centroid
 
+        # update again the points after centroids are updated
         for p in points:
             min_distance = 1000
             c_min = -1
@@ -47,6 +51,6 @@ if __name__ == "__main__":
     nb_iterations = 300
     points = np.array([[1, 2], [1.5, 1.8], [5, 8], [8, 8], [1, 0.6], [9, 11]])
     print(points.shape)
-    dict_centroid_points, centroids =  kmeans(points,nb_iterations, k)
+    dict_centroid_points, centroids = kmeans(points, nb_iterations, k)
     print(centroids)
     print(dict_centroid_points)
