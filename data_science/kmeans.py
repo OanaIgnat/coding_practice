73a7841f8467e5e6ea7fed942c464b8d71be9a55
OanaIgnat <oignat@umich.edu>
2022-02-25 13:04:42 -0500

    clean kmeans

--- data_science/kmeans.py
+++ data_science/kmeans.py
@@ -10,28 +10,34 @@ def euclidean_distance(point1, point2):
     return math.sqrt(sum((point1 - point2) ** 2))
 
 
-def kmeans(points, nb_iterations, k):
-    centroids = np.zeros((k, points.shape[1]))  # (#points, #coordinates)
-    for i in range(nb_iterations):
+def kmeans(data_points, nb_iterations, k):
+    # assign random centroids
+    centroids = np.zeros((k, data_points.shape[1]))  # (#points, #coordinates)
+
+    for _ in range(nb_iterations):
         dict_centroid_points = {}
-        for p in points:
+
+        # assign each point to it's nearest centroid
+        for point in data_points:
             min_distance = 1000
             c_min = -1
-            for index_c, c in enumerate(centroids):
-                dist = euclidean_distance(c, p)
+            for index, centroid in enumerate(centroids):
+                dist = euclidean_distance(centroid, point)
                 if dist <= min_distance:
                     min_distance = dist
-                    c_min = index_c
+                    c_min = index
             if c_min not in dict_centroid_points.keys():
                 dict_centroid_points[c_min] = []
-            dict_centroid_points[c_min].append(p)
+            dict_centroid_points[c_min].append(point)
 
+
+        # recompute the centroids
         for c in dict_centroid_points.keys():
-            new_centroid = np.mean(dict_centroid_points[c], axis=0) # mean over the coordinates
+            new_centroid = np.mean(dict_centroid_points[c], axis=0)  # mean over the coordinates
             centroids[c] = new_centroid
 
         # update again the points after centroids are updated
-        for p in points:
+        for p in data_points:
             min_distance = 1000
             c_min = -1
             for index_c, c in enumerate(centroids):
@@ -51,6 +57,7 @@ if __name__ == "__main__":
     nb_iterations = 300
     points = np.array([[1, 2], [1.5, 1.8], [5, 8], [8, 8], [1, 0.6], [9, 11]])
     print(points.shape)
+
     dict_centroid_points, centroids = kmeans(points, nb_iterations, k)
     print(centroids)
     print(dict_centroid_points)
